18a3ec30a81e4c23d6b794d6d7e0fae9836243d0
Mariengs <marianne.waago@hotmail.com>
2024-09-05 13:16:41 +0200

    2 tasks ok

--- js/ui/products/createProducts.js
+++ js/ui/products/createProducts.js
@@ -1,15 +1,38 @@
 export function createProducts(container, products) {
-  for (let i = 0; i < products.data.length; i++) {
-    const product = products[i];
+  console.log(products);
+  console.log(products.data.length);
 
+  for (let i = 0; i < products.data.length; i++) {
+    const product = products.data[i];
     const div = document.createElement("div");
-    div.classList.add("products");
+    div.classList.add("product");
 
     const h2 = document.createElement("h2");
-    h2.textContent = products.title;
+    h2.textContent = product.title;
 
     const p = document.createElement("p");
-    p.textContent = products.description;
+    p.textContent = product.description;
+
+    if (product.image && product.image.url) {
+      const img = document.createElement("img");
+      img.src = product.image.url;
+      img.alt = product.image.alt || product.title;
+      div.append(img);
+    } else {
+      console.warn(`No image available for product: ${product.title}`);
+    }
+
+    if (product.gender) {
+      const genderP = document.createElement("p");
+      genderP.textContent = `Gender: ${product.gender}`;
+      div.append(genderP);
+    }
+
+    if (product.sizes && product.sizes.length > 0) {
+      const sizesP = document.createElement("p");
+      sizesP.textContent = `Available sizes: ${product.sizes.join(", ")}`;
+      div.append(sizesP);
+    }
 
     div.append(h2);
     div.append(p);
